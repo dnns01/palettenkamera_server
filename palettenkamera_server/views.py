1498fb8b1fb14b3d3d8fc9947267053b319f18c7
Dennis Klein <github@dnns01.de>
2020-10-14 14:54:01 +0200

    Fix minor issues in views.py

--- palettenkamera_server/views.py
+++ palettenkamera_server/views.py
@@ -6,19 +6,19 @@ import os
 def home(request):
     with picamera.PiCamera() as camera:
         camera.resolution = (3280, 2464)
-        camera.capture("/var/www/palettenkamera/media/bild.jpg")
+        camera.capture("/var/www/palettenkamera_server/media/bild.jpg")
     return render(request, 'home.html', {'bild': 'bild.jpg'})
 
 
 def home_no(request, no):
     with picamera.PiCamera() as camera:
         camera.resolution = (3280, 2464)
-        camera.capture("/var/www/palettenkamera/media/" + no + ".jpg")
-    return render(request, 'home.html', {'bild': no + '.jpg'})
+        camera.capture("/var/www/palettenkamera_server/media/" + no + ".jpg")
+    return render(request, 'home.html', {'bild': str(no) + '.jpg'})
 
 
 def delete(request, no):
-    path = "/var/www/palettenkamera/media/" + no + ".jpg"
+    path = "/var/www/palettenkamera_server/media/" + str(no) + ".jpg"
     if os.path.isfile(path):
         os.remove(path)
     return render(request, 'home.html', {})
